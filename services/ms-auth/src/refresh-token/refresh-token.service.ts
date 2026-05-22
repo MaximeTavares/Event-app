248bf78418066f5984bdce6b6e9e06082e4de08b
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 13:05:43 +0200

    feat(ms-auth): Add RefreshTokenService

---
+++ services/ms-auth/src/refresh-token/refresh-token.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { RefreshToken } from './refresh-token.schema';
+import { Model } from 'mongoose';
+import { compare, hash } from 'src/utils/password.util';
+
+@Injectable()
+export class RefreshTokenService {
+    constructor(
+        @InjectModel(RefreshToken.name)
+        private readonly refreshTokenModel: Model<RefreshToken>,
+    ) {}
+
+    async save(userId: string, refreshToken: string) {
+        const hashed = await hash(refreshToken);
+
+        await this.refreshTokenModel.deleteMany({ userId });
+
+        return this.refreshTokenModel.create({
+            userId,
+            tokenHash: hashed,
+            revoked: false,
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+    }
+
+    async validate(userId: string, token: string): Promise<boolean> {
+        const tokenInDb = await this.refreshTokenModel.findOne({
+            userId,
+            revoked: false,
+        });
+
+        if (!tokenInDb) return false;
+
+        return compare(token, tokenInDb.tokenHash);
+    }
+
+    async revoke(userId: string) {
+        await this.refreshTokenModel.updateMany({ userId }, { revoked: true });
+    }
+
+    async rotate(userId: string, oldToken: string, newToken: string) {
+        const valid = await this.validate(userId, oldToken);
+
+        if (!valid) return;
+
+        await this.save(userId, newToken);
+    }
+}
