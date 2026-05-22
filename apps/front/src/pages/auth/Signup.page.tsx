3fe49f5aacdc6357c0ae68f6ee3b346a9edec4b1
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 12:59:36 +0200

    feat(Front): Types changes

--- apps/front/src/pages/auth/Signup.page.tsx
+++ apps/front/src/pages/auth/Signup.page.tsx
@@ -1,10 +1,10 @@
-import { SignUpForm } from "../../features/auth/components/SignUpForm";
-import { FormLayout } from "../../shared/layout/FormLayout";
+import { SignUpForm } from '../../features/auth/components/SignUpForm';
+import { FormLayout } from '../../shared/layout/FormLayout';
 
 export default function SignupPage() {
-	return (
-		<FormLayout title={"Inscription"}>
-			<SignUpForm />
-		</FormLayout>
-	);
+    return (
+        <FormLayout title={'Inscription'}>
+            <SignUpForm />
+        </FormLayout>
+    );
 }
