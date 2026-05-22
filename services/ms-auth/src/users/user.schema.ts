0ff42f79052b38a1c615d8a496d4e7075dbe00cc
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 13:06:05 +0200

    feat: Add UserService

--- services/ms-auth/src/users/user.schema.ts
+++ services/ms-auth/src/users/user.schema.ts
@@ -1,18 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { type Role } from 'src/type/auth.type';
 
-export type AuthUserDocument = HydratedDocument<AuthUser>;
+export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
-export class AuthUser {
+export class User {
     @Prop({ required: true, unique: true })
     email: string;
 
     @Prop({ required: true })
     password: string;
 
-    @Prop()
-    roles: string[];
+    @Prop({ default: 'USER' })
+    role: Role;
 }
 
-export const AuthUserSchema = SchemaFactory.createForClass(AuthUser);
+export const UserSchema = SchemaFactory.createForClass(User);
