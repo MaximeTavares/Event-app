17c6b248ce3c63ddb2aaa78124487005e71b0c35
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 12:58:40 +0200

    feat(Front): Update Signin and Signup form

--- apps/front/src/features/auth/components/SignUpForm.tsx
+++ apps/front/src/features/auth/components/SignUpForm.tsx
@@ -1,88 +1,93 @@
-import { useForm, type SubmitHandler } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Link } from "react-router";
-import { passwordRules, registerSchema } from "../validation/Signup.schema";
-import { OAuthButtons } from "./OAuthButtons";
-import { FormField } from "../../../shared/components/UI/formField/FormField";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link, useNavigate } from 'react-router';
+import { passwordRules, registerSchema } from '../validation/Signup.schema';
+import { OAuthButtons } from './OAuthButtons';
+import { FormField } from '../../../shared/components/UI/formField/FormField';
+import { useSignup } from '../hooks/use_auth.service';
+import type { SignupFormData } from '../types/types';
 
 type Inputs = {
-	email: string;
-	password: string;
-	confirmPassword: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
 };
 
 export function SignUpForm() {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm<Inputs>({
-		resolver: yupResolver(registerSchema),
-		mode: "onChange",
-	});
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<Inputs>({
+        resolver: yupResolver(registerSchema),
+        mode: 'onChange',
+    });
 
-	const password = watch("password", "");
+    const password = watch('password', '');
+    const navigate = useNavigate();
+    const signup = useSignup();
 
-	const rules = passwordRules(password);
+    const rules = passwordRules(password);
 
-	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		if (data.password === data.confirmPassword) {
-			console.log(data);
+    const onSubmit = async (data: SignupFormData) => {
+        if (data.password === data.confirmPassword) {
+            await signup.mutateAsync({
+                email: data.email,
+                password: data.password,
+            });
+            navigate('/auth/signin');
+        }
+    };
 
-			//TODOS Request http to backend
-			//Code...
-		}
-	};
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <FormField
+                type="input"
+                label="Email"
+                error={errors.email?.message}
+                {...register('email')}
+            />
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-			<FormField
-				type="input"
-				label="Email"
-				error={errors.email?.message}
-				{...register("email")}
-			/>
+            <FormField
+                type="password"
+                label="Mot de passe"
+                error={errors.password?.message}
+                {...register('password')}
+            />
 
-			<FormField
-				type="password"
-				label="Mot de passe"
-				error={errors.password?.message}
-				{...register("password")}
-			/>
+            <div className="label">
+                <span className="label-text-alt text-base-content/70">
+                    Votre mot de passe doit contenir :
+                </span>
+            </div>
 
-			<div className="label">
-				<span className="label-text-alt text-base-content/70">
-					Votre mot de passe doit contenir :
-				</span>
-			</div>
+            <ul className="text-xs text-base-content/70 ml-5">
+                <li className={rules.length ? 'text-secondary' : 'text-base-content/70'}>
+                    {rules.length ? '✓' : '•'} au moins 8 caractères
+                </li>
+                <li className={rules.numbers ? 'text-secondary' : 'text-base-content/70'}>
+                    {rules.numbers ? '✓' : '•'} au moins 2 chiffres
+                </li>
+                <li className={rules.specials ? 'text-secondary' : 'text-base-content/70'}>
+                    {rules.specials ? '✓' : '•'} au moins 2 caractères spéciaux
+                </li>
+            </ul>
 
-			<ul className="text-xs text-base-content/70 ml-5">
-				<li className={rules.length ? "text-secondary" : "text-base-content/70"}>
-					{rules.length ? "✓" : "•"} au moins 8 caractères
-				</li>
-				<li className={rules.numbers ? "text-secondary" : "text-base-content/70"}>
-					{rules.numbers ? "✓" : "•"} au moins 2 chiffres
-				</li>
-				<li className={rules.specials ? "text-secondary" : "text-base-content/70"}>
-					{rules.specials ? "✓" : "•"} au moins 2 caractères spéciaux
-				</li>
-			</ul>
+            <FormField
+                type="password"
+                label="Confirmation mot de passe"
+                error={errors.confirmPassword?.message}
+                {...register('confirmPassword')}
+            />
 
-			<FormField
-				type="password"
-				label="Confirmation mot de passe"
-				error={errors.confirmPassword?.message}
-				{...register("confirmPassword")}
-			/>
+            <button className="btn btn-neutral mt-2 w-full">S'inscrire</button>
 
-			<button className="btn btn-neutral mt-2 w-full">S'inscrire</button>
+            <OAuthButtons />
 
-			<OAuthButtons />
-
-			<Link to={"/auth/signin"}>
-				<p className="flex justify-center">Déjà un compte ?</p>
-			</Link>
-		</form>
-	);
+            <Link to={'/auth/signin'}>
+                <p className="flex justify-center">Déjà un compte ?</p>
+            </Link>
+        </form>
+    );
 }
