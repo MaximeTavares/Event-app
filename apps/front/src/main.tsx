58097c485262750feca78eadcb0d96631423d9e6
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 12:55:52 +0200

    feat(Front): Add Auth provider to main.tsx

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,19 +1,22 @@
-import { BrowserRouter } from "react-router";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "leaflet/dist/leaflet.css";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from 'react-router';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import 'leaflet/dist/leaflet.css';
+import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './features/auth/components/auth.provider.tsx';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</BrowserRouter>
-	</StrictMode>,
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </QueryClientProvider>
+        </BrowserRouter>
+    </StrictMode>,
 );
