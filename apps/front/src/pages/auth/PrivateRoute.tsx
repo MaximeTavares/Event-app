d87882aa22414a785116801a69097fe036c73f47
Maxime Tavares <t.maxime59@gmail.com>
2026-05-22 16:41:38 +0200

    feat(Front): Change old User form store to ReactQuery

--- apps/front/src/pages/auth/PrivateRoute.tsx
+++ apps/front/src/pages/auth/PrivateRoute.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from 'react-router';
-import { useAuthStore } from '../../features/auth/store/auth.store';
+import { useMe } from '../../features/auth/hooks/use_auth.service';
 
 type Props = {
     allowedRoles?: string[];
 };
 
 export default function PrivateRoute({ allowedRoles }: Readonly<Props>) {
-    const user = useAuthStore((state) => state.user);
+    const { data: user } = useMe();
 
     // Si pas connécté
     if (!user) return <Navigate to="/" replace />;
