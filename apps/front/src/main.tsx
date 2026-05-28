a993605401d4ffc3a49146beb3a475388ac44cd1
Maxime Tavares <t.maxime59@gmail.com>
2026-05-28 09:07:42 +0200

    feat(Front): Init OAuth google

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -6,17 +6,20 @@ import 'leaflet/dist/leaflet.css';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './features/auth/components/auth.provider.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <AuthProvider>
-                    <App />
-                </AuthProvider>
-            </QueryClientProvider>
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <AuthProvider>
+                        <App />
+                    </AuthProvider>
+                </QueryClientProvider>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
     </StrictMode>,
 );
