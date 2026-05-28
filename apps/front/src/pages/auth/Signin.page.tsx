b681b09cf2faae7bbf1f2b3b396bdc957dcaf647
Maxime Tavares <t.maxime59@gmail.com>
2026-05-28 09:08:19 +0200

    feat(Infra): Add Google Oauth button

--- apps/front/src/pages/auth/Signin.page.tsx
+++ apps/front/src/pages/auth/Signin.page.tsx
@@ -1,10 +1,33 @@
+import { GoogleLogin, type CredentialResponse } from '@react-oauth/google';
 import { SignInForm } from '../../features/auth/components/SignInForm';
 import { FormLayout } from '../../shared/layout/FormLayout';
+import { AuthApi } from '../../features/auth/api/auth.api';
 
 export default function SigninPage() {
+    const onGoogleLogin = async (credentialsResponse: CredentialResponse) => {
+        try {
+            const idToken = credentialsResponse.credential;
+
+            if (!idToken) throw new Error('Missing google token');
+
+            const data = await AuthApi.googleSignin(idToken);
+
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <FormLayout title="Connexion">
-            <SignInForm />
-        </FormLayout>
+        <>
+            <FormLayout title="Connexion">
+                <SignInForm />
+            </FormLayout>
+
+            <GoogleLogin
+                onSuccess={(credentialsResponse) => onGoogleLogin(credentialsResponse)}
+                onError={() => console.log('Login failed')}
+            ></GoogleLogin>
+        </>
     );
 }
