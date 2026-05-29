cc255724c9a28da2cfb5394f57358916cfdb7eeb
Maxime Tavares <t.maxime59@gmail.com>
2026-05-29 22:28:50 +0200

    rollback prisma config with mariadb adapter

--- apps/gateway/prisma/prisma.service.ts
+++ apps/gateway/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const url = process.env.DATABASE_URL;
+
+        if (!url) {
+            throw new Error('DATABASE_URL missing');
+        }
+
+        const adapter = new PrismaMariaDb(url);
+
+        super({ adapter });
     }
 }
