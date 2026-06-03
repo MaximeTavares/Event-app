890e407bcea19f77fa982837868f2e557e43ace6
Maxime Tavares <t.maxime59@gmail.com>
2026-06-03 14:49:52 +0200

    refacto duplication googleauth

--- apps/front/src/pages/auth/Signup.page.tsx
+++ apps/front/src/pages/auth/Signup.page.tsx
@@ -1,39 +1,19 @@
-import { GoogleLogin, type CredentialResponse } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import { SignUpForm } from '../../features/auth/components/SignUpForm';
 import { Card } from '../../shared/layout/Card';
 import { PageContainer } from '../../shared/layout/PageContainer';
-import { useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router';
-import { useGoogleSignin } from '../../features/auth/hooks/use_auth.service';
+import { useGoogleAuth } from '../../features/auth/hooks/useGoogleAuth';
 
 export default function SignupPage() {
-    const navigate = useNavigate();
-    const queryClient = useQueryClient();
-    const signin = useGoogleSignin();
+    const { googleLogin } = useGoogleAuth();
 
-    const onGoogleLogin = async (credentialsResponse: CredentialResponse) => {
-        try {
-            const idToken = credentialsResponse.credential;
-
-            if (!idToken) throw new Error('Missing google token');
-
-            await signin.mutateAsync({ idToken });
-            await queryClient.invalidateQueries({
-                queryKey: ['me'],
-            });
-
-            navigate('/');
-        } catch (error) {
-            console.log(error);
-        }
-    };
     return (
         <PageContainer>
             <Card title="S'inscrire" size="lg">
                 <SignUpForm />
                 <div className="flex justify-center items-center p-3">
                     <GoogleLogin
-                        onSuccess={(credentialsResponse) => onGoogleLogin(credentialsResponse)}
+                        onSuccess={(credentialsResponse) => googleLogin(credentialsResponse)}
                         onError={() => console.log('Login failed')}
                     ></GoogleLogin>
                 </div>
