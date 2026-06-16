ac7efb14a6cc3b27ea32872b8bfc1c796c05913d
Maxime Tavares <t.maxime59@gmail.com>
2026-06-16 19:19:15 +0200

    Change prisma config

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -8,7 +8,10 @@ const adapter = new PrismaMariaDb({
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
-	connectionLimit: 5,
+    connectionLimit: 5,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
